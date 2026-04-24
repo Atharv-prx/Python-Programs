a18ea862d75e349e3987c4837ddf0ad65517ed13
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-24 22:10:19 +0530

    Nothing Crazy

---
+++ Basic-Programs/Area_calc.py
@@ -0,0 +1,9 @@
+length = float(input("Enter length of rectangle "))
+width = float(input("Enter width of rectangle "))
+Height = float(input("Enter height of rectangle "))
+
+area = length * width 
+Volume = Height * area
+
+print (f"Area of rectangle is {area}")
+print (f"Volume of rectangle is {Volume}")
