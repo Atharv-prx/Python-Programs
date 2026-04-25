bf661259efd3704376a5c4cc00d9926d755c3c4d
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-25 15:07:45 +0530

    Not too shaby

---
+++ String/Email-slicer.py
@@ -0,0 +1,8 @@
+email = input("What's your email: ")
+
+index = email.index("@")
+
+username = email[:index]
+domain = email[index+1 :]
+
+print(f"Your username is {username} and domain is {domain}")
