923c05438e4a894b815c48324944de970cccda57
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-08 18:05:06 +0530

    Gonna add validation

---
+++ Mini-Projects/11-Calculator-with-Magic-Methods.py
@@ -0,0 +1,22 @@
+class Calculator:
+    def __init__(self, num):
+        self.num = num
+    
+    def __add__(self, other):
+        return self.num + other.num
+    
+    def __sub__(self, other):
+        return self.num - other.num
+    
+    def __mul__(self, other):
+        return self.num * other.num
+    
+    def __truediv__(self, other):
+        return self.num / other.num
+num1 = Calculator(5)
+num2 = Calculator(3)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
