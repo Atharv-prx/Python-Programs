b2f85bef324d7b94b394627a6973a5979c6b8d40
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-19 16:23:04 +0530

    Now i can finnaly add other conics without messing up others

---
+++ Mini-Projects/15-Conic-Section/Final-Version/Main.py
@@ -0,0 +1,30 @@
+from Helpers import get_choice
+from Ellipse import ellipse_program
+
+def main():
+
+    while True:
+
+        print("\n===========================")
+        print("Main Menu - Conic Sections")
+        print("===========================")
+        print("1. Circle")
+        print("2. Parabola")
+        print("3. Ellipse")
+        print("4. Hyperbola")
+        print("5. Exit")
+
+        choice = get_choice("Choose an option: ", 1, 5)
+
+        if choice == 3:
+            ellipse_program()
+
+        elif choice == 5:
+            print("Exiting program.")
+            break
+
+        else:
+            print("Feature coming soon.")
+
+if __name__ == "__main__":
+    main()
