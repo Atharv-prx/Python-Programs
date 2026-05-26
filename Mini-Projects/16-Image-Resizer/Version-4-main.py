881f7d8612b427d5bdbd0541418396a83e6bd168
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-26 21:25:07 +0530

    OMG this fking program suckes, took me too long to make ts 🥀

---
+++ Mini-Projects/16-Image-Resizer/Version-4-main.py
@@ -0,0 +1,81 @@
+from PIL import Image
+import os
+import threading
+import queue
+
+q = queue.Queue()
+print_lock = threading.Lock()
+
+# Ask user for target format
+target_format = input("Enter output format (png/jpg/jpeg/webp): ").lower()
+
+# Allowed formats
+allowed_formats = ["png", "jpg", "jpeg", "webp"]
+
+if target_format not in allowed_formats:
+    print("Unsupported format.")
+    exit()
+
+# Process one image
+def resize_image(file):
+
+    path = f"input/{file}"
+
+    img = Image.open(path)
+
+    # Resize image
+    img.thumbnail((800, 600))
+
+    # Split filename and extension
+    filename, old_extension = os.path.splitext(file)
+
+    # JPG/JPEG doesn't support transparency
+    if target_format in ["jpg", "jpeg"]:
+        img = img.convert("RGB")
+
+    # Create new output path
+    output_path = f"output/{filename}.{target_format}"
+
+    # Save converted image
+    img.save(output_path)
+
+    with print_lock:
+        print(
+            f"{file} converted to "
+            f"{target_format} successfully."
+        )
+
+# Worker thread
+def worker():
+
+    while not q.empty():
+
+        file = q.get()
+
+        resize_image(file)
+
+        q.task_done()
+
+# Add jobs to queue
+for file in os.listdir("input"):
+    q.put(file)
+
+# Create worker threads
+threads = []
+
+for i in range(4):
+
+    t = threading.Thread(target=worker)
+
+    threads.append(t)
+
+    t.start()
+
+# Wait for all queue tasks
+q.join()
+
+# Wait for all threads
+for t in threads:
+    t.join()
+
+print("\nAll images processed.")
